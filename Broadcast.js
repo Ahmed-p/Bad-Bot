2f331a7ef8878645f9425942a6a9d44057bef2e6
Ahmed-p <35038073+Ahmed-p@users.noreply.github.com>
2018-01-02 20:05:27 +0200

    Update Broadcast.js

--- Broadcast.js
+++ Broadcast.js
@@ -1,14 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login('MzkyMDAyMDgxNDUzNjM3NjMy.DS1ZJg.ZxbOF2XMlJ8ATWp6Kive6DzqYUY');
+// THIS  MUST  BE  THIS  WAY
+client.login(MzkyMDAyMDgxNDUzNjM3NjMy.DS1ZJg.ZxbOF2XMlJ8ATWp6Kive6DzqYUY);
