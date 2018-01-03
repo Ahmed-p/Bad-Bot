a76a86c6b739ff33f1143f05819d475c98f7d538
Ahmed-p <35038073+Ahmed-p@users.noreply.github.com>
2018-01-03 15:27:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,24 @@ client.on('message', message => {
   	}
 });
 
+    client.on('message', message => {
+     if (message.content === "id") {
+     let embed = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)  
+  .setAuthor(message.author.username)
+.setDescription("معلومات عن الحــساب")
+               .setFooter(`Ahmed Bot.`, '')
+  .setColor("#9B59B6")
+  .addField("اســـم الحســاب", `${message.author.username}`)
+  .addField('كود الحساب الخاص', message.author.discriminator)
+  .addField("الرقـــم الشـــخصي", message.author.id)
+  .addField('بــــوت', message.author.bot)
+  .addField("تاريخ التسجيل", message.author.createdAt)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
