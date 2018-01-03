298428440c865f475a19203926c638e5bc92f284
Ahmed-p <35038073+Ahmed-p@users.noreply.github.com>
2018-01-03 15:54:15 +0200

    Update bad bot.js

--- bad bot.js
+++ bad bot.js
@@ -16,51 +16,5 @@ client.on('message', message => {
     	message.reply('BONG!');
   	}
 });
-
-client.on('message', message => {
-	if (message.content === "id") {
-	let embed = new Discord.RichEmbed()
- .setThumbnail(message.author.avatarURL)  
- .setAuthor(message.author.username)
-.setDescription("معلومات عن الحــساب")
-			  .setFooter(`Ahmed Bot.`, '')
- .setColor("#9B59B6")
- .addField("اســـم الحســاب", `${message.author.username}`)
- .addField('كود الحساب الخاص', message.author.discriminator)
- .addField("الرقـــم الشـــخصي", message.author.id)
- .addField('بــــوت', message.author.bot)
- .addField("تاريخ التسجيل", message.author.createdAt)
-	
-	
- message.channel.sendEmbed(embed);
-   }
-});
-
-client.on('message', message => {
- if(message.content === 'server') {
-	 var servername = message.guild.name
-	 var اونر = message.guild.owner
-	 var اعضاء = message.guild.memberCount
-	 var ايدي = message.guild.id
-	 var بلدالسيرفر = message.guild.region
-	 var الرومات = message.guild.channels.size
-	 var الرتب = message.guild.roles
-	 var عمل = message.guild.createdAt
-	 var الروم = message.guild.defaultChannel
-	 var server = new Discord.RichEmbed()
-	 .setThumbnail(message.guild.iconURL)
-	 .addField('اسم السيرفر', servername)
-	 .addField('اي دي السيرفر ' , [ايدي])
-	 .addField('أعضاء السيرفر', اعضاء)
-	 .addField('رومات السيرفر', الرومات)
-	 .addField('روم الشات الأساسي', الروم)
-	 .addField('صاحب السيرفر', اونر)
-	 .addField('بلد السيرفر', بلدالسيرفر)
-	 .addField('تاريخ افتتاح السيرفر', عمل)
-	 .setColor('RANDOM')
-
-	 message.channel.sendEmbed(server)
- }
-});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
